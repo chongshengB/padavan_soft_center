0d9036daed20a0de096483ced6cdf954d0d17097
chongshengB <53459145+chongshengB@users.noreply.github.com>
2020-04-08 14:29:13 +0800

    upload test

---
+++ app.json.js
@@ -0,0 +1,28 @@
+{
+    "apps": [
+        {
+            "description": "adguardhome", 
+            "md5": "bb2c87aed6b7c3580dee839c40fbbd29", 
+            "name": "adguardhome", 
+            "tar_url": "adguardhome/adguardhome.tar.gz", 
+            "title": "adguardhome", 
+            "version": "1.0.0"
+        }, 
+        { 
+            "description": "科学上网", 
+            "md5": "5cf48ecc7f8e056c8e632456e694f0cf", 
+            "name": "shadowsocks", 
+            "tar_url": "shadowsocks/shadowsocks.tar.gz", 
+            "title": "科学上网", 
+            "version": "1.0.1"
+        }, 
+        {
+            "description": "adbyby plus", 
+            "md5": "5cf48ecc7f8e056c8e632456e694f0cf", 
+            "name": "adbyby", 
+            "tar_url": "adbyby/adbyby.tar.gz", 
+            "title": "Adbyby plus+", 
+            "version": "1.0.1"
+        }
+    ]
+}
