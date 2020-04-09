c6c7c8088659dc785e02d951030354bd943c442a
chongshengB <53459145+chongshengB@users.noreply.github.com>
2020-04-09 09:30:11 +0800

    Update app.json.js

--- app.json.js
+++ app.json.js
@@ -14,7 +14,7 @@
             "name": "shadowsocks", 
             "tar_url": "shadowsocks/shadowsocks.tar.gz", 
             "title": "科学上网", 
-            "version": "1.1.2"
+            "version": "1.1.3"
         }, 
         {
             "description": "adbyby plus", 
